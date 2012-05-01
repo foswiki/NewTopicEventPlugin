68c51390c828af533c38e82c5f8c6d4e20f4aee9
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/NewTopicEventPlugin@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/NewTopicEventPlugin/build.pl
+++ lib/TWiki/Plugins/NewTopicEventPlugin/build.pl
@@ -3,14 +3,14 @@
 # Build for NewTopicEventPlugin
 #
 BEGIN {
-    unshift @INC, split(/:/, $ENV{FOSWIKI_LIBS});
+    unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} );
 }
 
 use TWiki::Contrib::Build;
 
 # Create the build object
-$build = new TWiki::Contrib::Build( 'NewTopicEventPlugin' );
+$build = new TWiki::Contrib::Build('NewTopicEventPlugin');
 
 # Build the target on the command line, or the default target
-$build->build($build->{target});
+$build->build( $build->{target} );
 
